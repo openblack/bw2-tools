52eb659d01c4528768a701b4785c9aca54d571bd
Matt Stevens <matt@dev-zilla.net>
2014-04-06 16:13:51 +0100

    a little error checking in the program against invalid args.

--- src/app/chlextract/Program.cs
+++ src/app/chlextract/Program.cs
@@ -11,13 +11,28 @@ namespace ScriptReader
 {
     class Program
     {
-        static void Main(string[] args)
+        public static int Main(string[] args)
         {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: chlex [in.chl] [out.chl]");
+                return 1;
+            }
+
+            if (!File.Exists(args[0]))
+            {
+                Console.WriteLine("{0} does not exist!", args[0]);
+                return 1;
+            }
+
             var chlFile = ChallengeFile.LoadFromStream(new FileStream(args[0], FileMode.Open));
 
+            Directory.CreateDirectory(Path.GetDirectoryName(args[1]));
             var file = File.CreateText(args[1]);
             ASMOut.Convert(chlFile, file);
             file.Close();
+
+            return 0;
         }
     }
 }
